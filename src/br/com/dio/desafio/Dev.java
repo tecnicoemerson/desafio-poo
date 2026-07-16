7041e1e8234eda547c2773a8141c3810615b41ea
tecnicoemerson <emersonrodrigues5578@gmail.com>
2026-07-16 10:50:47 -0300

    Herenca e polimorfismo parte 2

--- src/br/com/dio/desafio/Dev.java
+++ src/br/com/dio/desafio/Dev.java
@@ -1,4 +1,55 @@
 package br.com.dio.desafio;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscricao = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void InscreverBootcamp(Bootcamp bootcamp) {
+    }
+
+    public void progredir(){
+    }
+
+    public void calcularTotalXp(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscricao() {
+        return conteudosInscricao;
+    }
+
+    public void setConteudosInscricao(Set<Conteudo> conteudosInscricao) {
+        this.conteudosInscricao = conteudosInscricao;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscricao, dev.conteudosInscricao) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscricao, conteudosConcluidos);
+    }
 }
